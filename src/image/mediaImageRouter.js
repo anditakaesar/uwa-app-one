8681450db506daf91c5caf7a05621e747c4eb4fa
Andita Kaesar <anditakaesar@gmail.com>
2020-02-20 14:50:08 +0700

    improve signup security

--- src/image/mediaImageRouter.js
+++ src/image/mediaImageRouter.js
@@ -29,7 +29,7 @@ router.get('/:id', (req, res, next) => {
             res.status(200).json({
                 id: req.params.id,
                 mediaImage
-            })
+            });
         });
     });
 }); // router.get(id)
